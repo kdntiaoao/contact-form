4ba6f2fb882450381142dc4e9e328f963062a79c
iadokonat <iadokonat0106@gmail.com>
2022-09-07 20:48:53 +0900

    add mui

---
+++ src/themes/index.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ff64cd',
+      main: '#ed219c',
+      dark: '#b6006e',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#70ffff',
+      main: '#24e0ce',
+      dark: '#00ad9d',
+      contrastText: '#000',
+    },
+  },
+})
