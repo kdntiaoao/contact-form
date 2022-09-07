1db93c87084301a8f98898c88752ffbba84f3f71
iadokonat <iadokonat0106@gmail.com>
2022-09-07 21:11:53 +0900

    add _document.tsx and edit _app.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+import { theme } from 'themes'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
