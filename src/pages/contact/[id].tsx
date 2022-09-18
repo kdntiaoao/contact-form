0c7eb873e3ce80438a0ea4983ea330fcb25ed920
iadokonat <iadokonat0106@gmail.com>
2022-09-18 19:54:58 +0900

    add ChatForm component

--- src/pages/contact/[id].tsx
+++ src/pages/contact/[id].tsx
@@ -1,14 +1,16 @@
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext, NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { memo, useEffect } from 'react'
+import { memo, useEffect, useState } from 'react'
 
-import { Backdrop, Box, CircularProgress, Container, Typography } from '@mui/material'
+import { Backdrop, Box, CircularProgress, Container, Stack, Typography } from '@mui/material'
 import { signInAnonymously } from 'firebase/auth'
+import { doc, onSnapshot, Unsubscribe } from 'firebase/firestore'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
-import { auth } from '../../../firebase/client'
+import { auth, db } from '../../../firebase/client'
 import { adminDb } from '../../../firebase/server'
 
+import { ChatForm } from 'components/organisms/ChatForm'
 import { DefaultLayout } from 'components/template/DefaultLayout'
 import { getContactInfo } from 'services/getContactData'
 import { ChatData, ContactInfo } from 'types/data'
@@ -21,9 +23,10 @@ type ContactChatPageProps = {
 
 // eslint-disable-next-line react/display-name
 const ContactChatPage: NextPage<ContactChatPageProps> = memo(
-  ({ contactId, contactInfo, chatData }: ContactChatPageProps) => {
+  ({ contactId, contactInfo, chatData: initialChatData }: ContactChatPageProps) => {
     const router = useRouter()
     const [user] = useAuthState(auth)
+    const [chatData, setChatData] = useState<ChatData | undefined>(initialChatData)
 
     useEffect(() => {
       if (!user)
@@ -33,17 +36,38 @@ const ContactChatPage: NextPage<ContactChatPageProps> = memo(
     }, [user])
 
     useEffect(() => {
+      let unsub: Unsubscribe | undefined
+
       if (user && contactId) {
+        // お問い合わせ情報の取得
         getContactInfo(contactId).then((contactInfo) => {
           if (contactInfo) {
-            console.log('getContactInfo', ' => ', contactInfo)
+            // console.log('getContactInfo', ' => ', contactInfo)
           } else {
             console.log('contactInfo not exist')
+          }
+        })
+
+        // 現在のチャットデータの取得
+        unsub = onSnapshot(doc(db, 'chatData', contactId), (doc) => {
+          if (doc.exists()) {
+            const source = doc.metadata.hasPendingWrites ? 'Local' : 'Server'
+            const chatData = doc.data() as ChatData
+            console.log(source, ' data: ', doc.data())
+            setChatData(chatData)
+          } else {
+            console.log('chatData not exist')
           }
         })
       } else {
         console.log('user or id not exist')
       }
+
+      const cleanup = () => {
+        unsub?.()
+      }
+
+      return cleanup
     }, [contactId, user])
 
     if (router.isFallback) {
@@ -65,7 +89,9 @@ const ContactChatPage: NextPage<ContactChatPageProps> = memo(
             <Typography variant="h5">商品種別：{contactInfo?.category || 'undefined'}</Typography>
             <Typography variant="h5">送信時間：{contactInfo?.submitTime || 'undefined'}</Typography>
             <Box mt={4}>
-              <Typography>現在の状態：{chatData?.currentStatus || 'undefined'}</Typography>
+              <Typography>
+                現在の状態：{chatData?.currentStatus !== undefined ? chatData.currentStatus : 'undefined'}
+              </Typography>
               {chatData?.chatHistory?.map(({ contributor, postTime, contents: { text, newStatus } }) => (
                 <Box key={postTime} mt={4}>
                   <Typography>投稿者 : {contributor || 'undefined'}</Typography>
@@ -76,6 +102,15 @@ const ContactChatPage: NextPage<ContactChatPageProps> = memo(
             </Box>
           </Box>
         </Container>
+
+        {/* 入力エリア */}
+        <Stack sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+          <Container sx={{ border: '1px solid #aaa' }}>
+            <Box px={4} py={2}>
+              <ChatForm contributor="0" contactId={contactId} />
+            </Box>
+          </Container>
+        </Stack>
       </DefaultLayout>
     )
   }
