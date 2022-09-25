47a1e3b91c56cbfc7b6c890b233ead7a60ff5d7b
iadokonat <iadokonat0106@gmail.com>
2022-09-25 15:04:26 +0900

    add contact contents tooltip

--- src/components/organisms/containers/ContactTableContainer/index.tsx
+++ src/components/organisms/containers/ContactTableContainer/index.tsx
@@ -1,30 +1,11 @@
 import { memo } from 'react'
 
-import { format } from 'date-fns'
-
 import { ContactTable } from 'components/organisms/presentations/ContactTable'
 import { ContactInfo } from 'types/data'
-
-type Data = Omit<ContactInfo, 'email'>
-
-export type HeadCell = {
-  id: keyof Data
-  label: string
-}
 
 type Props = {
   contactInfoList: Record<string, ContactInfo>
 }
-
-const headCells: HeadCell[] = [
-  { id: 'currentStatus', label: '対応状況' },
-  { id: 'name', label: '氏名' },
-  { id: 'tel', label: '電話番号' },
-  { id: 'category', label: '製品種別' },
-  { id: 'submitTime', label: 'お問い合わせ日時' },
-  { id: 'contents', label: 'お問い合わせ内容' },
-  { id: 'supporter', label: '担当者' },
-]
 
 // eslint-disable-next-line react/display-name
 export const ContactTableContainer = memo(({ contactInfoList }: Props) => {
@@ -34,17 +15,16 @@ export const ContactTableContainer = memo(({ contactInfoList }: Props) => {
       name,
       tel,
       category,
-      contents: contents.length <= 100 ? contents : contents.slice(0, 100) + '...',
+      contents,
       supporter,
       currentStatus,
-      submitTime: format(submitTime, 'M月d日 H:mm'),
-      timestamp: submitTime,
+      submitTime,
       key: key,
     }
   })
   contactInfoArray.sort((a, b) => {
-    return a.currentStatus !== b.currentStatus ? a.currentStatus - b.currentStatus : a.timestamp - b.timestamp
+    return a.currentStatus !== b.currentStatus ? a.currentStatus - b.currentStatus : a.submitTime - b.submitTime
   })
 
-  return <ContactTable headCells={headCells} contactInfoArray={contactInfoArray} />
+  return <ContactTable contactInfoArray={contactInfoArray} />
 })
--- src/components/organisms/presentations/ContactTable/index.tsx
+++ src/components/organisms/presentations/ContactTable/index.tsx
@@ -1,12 +1,31 @@
 import Link from 'next/link'
 import { memo } from 'react'
 
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tooltip,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
+import { format } from 'date-fns'
 
-import { HeadCell } from 'components/organisms/containers/ContactTableContainer'
+import { ContactInfo } from 'types/data'
+
+type Data = Omit<ContactInfo, 'email'>
+
+type HeadCell = {
+  id: keyof Data
+  label: string
+}
 
 type Props = {
-  headCells: HeadCell[]
   contactInfoArray: {
     name: string
     tel: string
@@ -14,13 +33,46 @@ type Props = {
     contents: string
     supporter: string
     currentStatus: number
-    submitTime: string
+    submitTime: number
     key: string
   }[]
 }
 
+const headCells: HeadCell[] = [
+  { id: 'currentStatus', label: '状況' },
+  { id: 'name', label: '氏名' },
+  { id: 'tel', label: '電話番号' },
+  { id: 'category', label: '製品' },
+  { id: 'submitTime', label: '日時' },
+  { id: 'contents', label: 'お問い合わせ内容' },
+  { id: 'supporter', label: '担当' },
+]
+
 // eslint-disable-next-line react/display-name
-export const ContactTable = memo(({ headCells, contactInfoArray }: Props) => {
+export const ContactTable = memo(({ contactInfoArray }: Props) => {
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.up('md'))
+  const newContactInfoArray = contactInfoArray.map(
+    ({ name, tel, category, contents, supporter, currentStatus, submitTime, key }) => {
+      return {
+        name,
+        tel,
+        category,
+        contents,
+        formattedContents:
+          matches && contents.length > 100
+            ? contents.slice(0, 100) + '...'
+            : !matches && contents.length > 50
+            ? contents.slice(0, 50) + '...'
+            : contents,
+        supporter,
+        currentStatus,
+        submitTime: format(submitTime, matches ? 'M月d日 H:mm' : 'M/d'),
+        key,
+      }
+    }
+  )
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 750 }} aria-label="お問い合わせ一覧表">
@@ -35,33 +87,42 @@ export const ContactTable = memo(({ headCells, contactInfoArray }: Props) => {
         </TableHead>
 
         <TableBody>
-          {contactInfoArray.map(({ currentStatus, name, tel, category, contents, submitTime, supporter, key }) => (
-            <Link key={key} href={`/admin/contact/${key}`}>
-              <TableRow tabIndex={0} role="link" hover sx={{ cursor: 'pointer' }}>
-                <TableCell align="center">
-                  <Typography noWrap>{currentStatus}</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography noWrap>{name}</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography noWrap>{tel}</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography noWrap>{category}</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography noWrap>{submitTime}</Typography>
-                </TableCell>
-                <TableCell sx={{ minWidth: 300 }}>
-                  <Typography>{contents}</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography noWrap>{supporter}</Typography>
-                </TableCell>
-              </TableRow>
-            </Link>
-          ))}
+          {newContactInfoArray.map(
+            ({ currentStatus, name, tel, category, contents, formattedContents, submitTime, supporter, key }) => (
+              <Link key={key} href={`/admin/contact/${key}`}>
+                <TableRow tabIndex={0} role="link" hover sx={{ cursor: 'pointer' }}>
+                  <TableCell align="center">
+                    <Typography noWrap>{currentStatus}</Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography noWrap>{name}</Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography noWrap>{tel}</Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography noWrap>{category}</Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography noWrap>{submitTime}</Typography>
+                  </TableCell>
+                  <TableCell sx={{ minWidth: 300 }}>
+                    {/* 文字数が多い場合はもとの文章をツールチップで表示 */}
+                    {contents === formattedContents ? (
+                      <Typography>{formattedContents}</Typography>
+                    ) : (
+                      <Tooltip title={contents} arrow>
+                        <Typography>{formattedContents}</Typography>
+                      </Tooltip>
+                    )}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography noWrap>{supporter}</Typography>
+                  </TableCell>
+                </TableRow>
+              </Link>
+            )
+          )}
         </TableBody>
       </Table>
     </TableContainer>
