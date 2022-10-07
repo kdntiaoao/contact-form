af1d520b94cc6815de0dce34c9ac4d4ac98b8185
iadokonat <iadokonat0106@gmail.com>
2022-10-08 00:15:51 +0900

    test code

--- src/pages/sendGrid/index.tsx
+++ src/pages/sendGrid/index.tsx
@@ -1,12 +1,33 @@
-import { memo, useEffect, useState } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
 
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 
 import { DefaultLayout } from 'components/template/DefaultLayout'
 
 // eslint-disable-next-line react/display-name
 const SendGridPage = memo(() => {
   const [hello, setHello] = useState<string>()
+
+  const sendMail = useCallback(async () => {
+    await fetch('/api/send', {
+      body: JSON.stringify({
+        name: 'aaa',
+        email: 'swim.free.nthm@gmail.com',
+        tel: '080xxxxxxxx',
+        category: 'A001',
+        contents: 'aaaaa',
+        chatUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    })
+  }, [])
+
+  const handleClick = useCallback(() => {
+    sendMail()
+  }, [sendMail])
 
   useEffect(() => {
     const fetchHello = async () => {
@@ -28,6 +49,9 @@ const SendGridPage = memo(() => {
         <Typography variant="h2" textAlign="center">
           {hello || 'Error!!!'}
         </Typography>
+      </Box>
+      <Box py={6}>
+        <Button onClick={handleClick}>Send Mail</Button>
       </Box>
     </DefaultLayout>
   )
