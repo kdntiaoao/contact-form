830afdd48444a8faebc13ce08e0b126d4556b2e8
iadokonat <iadokonat0106@gmail.com>
2022-10-07 23:52:12 +0900

    test code

--- src/pages/sendGrid/index.tsx
+++ src/pages/sendGrid/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
 
 import { Box, Typography } from '@mui/material'
 
@@ -6,11 +6,27 @@ import { DefaultLayout } from 'components/template/DefaultLayout'
 
 // eslint-disable-next-line react/display-name
 const SendGridPage = memo(() => {
+  const [hello, setHello] = useState<string>()
+
+  useEffect(() => {
+    const fetchHello = async () => {
+      const res = await fetch('/api/hello')
+      const json = await res.json()
+      setHello(json.name)
+    }
+    fetchHello()
+  }, [])
+
   return (
     <DefaultLayout>
-      <Box sx={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+      <Box py={6}>
         <Typography variant="h2" textAlign="center">
           SendGrid
+        </Typography>
+      </Box>
+      <Box py={6}>
+        <Typography variant="h2" textAlign="center">
+          {hello || 'Error!!!'}
         </Typography>
       </Box>
     </DefaultLayout>
